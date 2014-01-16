4e3fe9c9f11d295732317b6748996320f6b5f150
HanbinKim <hottuc@naver.com>
2014-01-16 14:45:51 +0900

    Ex 5. 4. 7 / p 212
    Ex 5.4.7 /p 212완료하였습니다.

---
+++ Ex5.4.7/ViewController.m
@@ -0,0 +1,151 @@
+//
+//  ViewController.m
+//  Ex5.4.7
+//
+//  Created by SDT-1 on 2014. 1. 16..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate, AVAudioRecorderDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *status;
+@property (weak, nonatomic) IBOutlet UITableView *table;
+
+@end
+
+@implementation ViewController{
+    AVAudioRecorder *recorder;
+    NSMutableArray *recordingFiles;
+}
+
+
+
+-(NSString *)getuPullPath:(NSString *)fileName
+{
+    NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSLog(@"%@",fileName);
+    return [documentPath stringByAppendingPathComponent:fileName];
+}
+
+- (void)startRecording
+{
+    NSDate *date = [NSDate date];
+    NSString *filePath = [self getuPullPath:[NSString stringWithFormat:@"%@.caf",[date description]]];
+    NSLog(@"aaa:%@",[date description]);
+    NSLog(@"recording path : %@",filePath);
+    
+    NSURL *url = [NSURL fileURLWithPath:filePath];
+    NSLog(@"%@",url);
+    NSMutableDictionary *setting = [NSMutableDictionary dictionary];
+    [setting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [setting setValue:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+    
+    __autoreleasing NSError *error;
+    
+    recorder = [[AVAudioRecorder alloc]initWithURL:url settings:setting error:&error];
+    recorder.delegate = self;
+    
+    if([recorder prepareToRecord])
+    {
+        self.status.text = [NSString stringWithFormat:@"Recording:%@",[[url path] lastPathComponent]];
+        [recorder recordForDuration:10];
+    }
+    
+}
+
+- (void)updateRecodedFiles
+{
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    
+    __autoreleasing NSError *error = nil;
+    
+    recordingFiles = [[NSMutableArray alloc]initWithArray:[fm contentsOfDirectoryAtPath:documentPath error:&error]];
+    [self.table reloadData];
+}
+
+- (void)stopRecording
+{
+    [recorder stop];
+    [self updateRecodedFiles];
+}
+
+- (IBAction)toggleRecoding:(id)sender
+{
+    if([recorder isRecording])
+    {
+        [self stopRecording];
+        ((UIBarButtonItem *)sender).title = @"Record";
+    }
+    else
+    {
+        [self startRecording];
+        ((UIBarButtonItem *)sender).title = @"Stop";
+    }
+        
+}
+
+- (void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag
+{
+    self.status.text = @"녹음 완료";
+    [self updateRecodedFiles];
+}
+
+- (void)audioRecorderEncodeErrorDidOccur:(AVAudioRecorder *)recorder error:(NSError *)error
+{
+    self.status.text = [NSString stringWithFormat:@"녹음중 오류 :%@",[error description]];
+}
+
+#pragma mark Table..
+#define CELL_ID @"CELL_ID"
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recordingFiles count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    cell.textLabel.text = [recordingFiles objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *fileName = [recordingFiles objectAtIndex:indexPath.row];
+    NSString *fullPath = [self getuPullPath:fileName];
+    NSFileManager *fm = [NSFileManager defaultManager];
+    
+    __autoreleasing NSError *error = nil;
+    BOOL ret = [fm removeItemAtPath:fullPath error:&error];
+    
+    if(NO==ret)
+        NSLog(@"Error:%@",[error localizedDescription]);
+    
+    [recordingFiles removeObjectAtIndex:indexPath.row];
+    [self.table deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+    
+    
+    
+    
+    
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
